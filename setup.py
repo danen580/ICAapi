02fd0b3a59591680cf506a6c09000971c63acde1
danen580 <33568127+danen580@users.noreply.github.com>
2020-04-19 21:21:53 +0200

    added download_url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Daniel Engberg',                   # Type in your name
   author_email = 'engberg.daniel@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/danen580/ICAapi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/danen580/ICAapi/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['ICA', 'Shoppinglist'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'json',
